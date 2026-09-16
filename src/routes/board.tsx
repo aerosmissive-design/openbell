7f96c7f1e7305e10ffb406670f0f4e6f61bc2b15
aerosmissive-design <aerosmissive@gmail.com>
2026-09-16 22:17:46 +0900

    feat: add wide board route

---
+++ src/routes/board.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { BoardView } from "@/components/board-view";
+export const Route = createFileRoute("/board")({ component: BoardView });
