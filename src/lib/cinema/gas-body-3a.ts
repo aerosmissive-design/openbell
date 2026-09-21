bddd6fd1fa25f625a55799ca9b18c79de33ad0b2
aerosmissive-design <aerosmissive@gmail.com>
2026-09-22 00:16:43 +0900

    fix(gas): assemble GAS_BODY_3A from 3a1+3a2+3a3

---
+++ src/lib/cinema/gas-body-3a.ts
@@ -0,0 +1,4 @@
+import { GAS_BODY_3A1 } from "./gas-body-3a1";
+import { GAS_BODY_3A2 } from "./gas-body-3a2";
+import { GAS_BODY_3A3 } from "./gas-body-3a3";
+export const GAS_BODY_3A = GAS_BODY_3A1 + GAS_BODY_3A2 + GAS_BODY_3A3;
