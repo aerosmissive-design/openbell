ebac2cc79d1721d92a73b81a9c67815e897f4b1c
aerosmissive-design <aerosmissive@gmail.com>
2026-09-21 17:17:11 +0900

    feat(gas): add gas-body parts + body assembler (full template restore)

--- src/lib/cinema/gas-script-body.ts
+++ src/lib/cinema/gas-script-body.ts
@@ -5,5 +5,6 @@ import { P2 } from "./gas-body-p2";
 import { P3 } from "./gas-body-p3";
 import { P4 } from "./gas-body-p4";
 import { P5 } from "./gas-body-p5";
+import { P6 } from "./gas-body-p6";
 
-export const GAS_SCRIPT_BODY = P0 + P1 + P2 + P3 + P4 + P5;
+export const GAS_SCRIPT_BODY = P0 + P1 + P2 + P3 + P4 + P5 + P6;
